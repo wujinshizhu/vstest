33ae6085985522668129fe3a15733c8476de6a4b
wujin <1156351842@qq.com>
2014-03-12 09:58:17 +0800

    just try C#

---
+++ vstest/wujintest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vstest
+{
+    class wujintest
+    {
+        //I don't know what fuck is this,so just fuck it. 
+    }
+}
